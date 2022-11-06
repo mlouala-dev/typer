b4f80b20417342ecd041e4fe759aa131c4c0fc25
Mahdi <mlouala@gmail.com>
2022-11-06 19:51:03 +0100

    adding shortcut to zoomIn / out

--- UI/Editor.py
+++ UI/Editor.py
@@ -978,8 +978,8 @@ class Typer(QTextEdit):
             elif key == Qt.Key.Key_E:
                 self.insertHtml('&#x200e;')
 
-            elif key == Qt.Key.Key_Equal or ord(current_text) == 31:
-                self.zoom(3 if key == Qt.Key.Key_Equal else -3)
+            elif current_text == '=' or ord(current_text) == 31:
+                self.zoom(5 if current_text == '=' else -5)
 
             # otherwise we just consider it a normal command
             else:
@@ -1117,8 +1117,8 @@ class Typer(QTextEdit):
             tc.endEditBlock()
             return
 
-        elif modifiers == Qt.KeyboardModifier.ControlModifier and current_text in (Qt.Key.Key_Equal, Qt.Key.Key_Minus):
-            self.zoom(1 if current_text == Qt.Key.Key_Equal else -1)
+        elif modifiers == Qt.KeyboardModifier.ControlModifier and current_text in '-=':
+            self.zoom(1 if current_text == '=' else -1)
 
         # forwarding to parent to jump through pages
         elif key in (Qt.Key.Key_PageUp, Qt.Key.Key_PageDown):
